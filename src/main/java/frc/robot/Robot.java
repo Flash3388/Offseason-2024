c45ca05b3ff1285e14fa9ec6c5820b922ba63cfc
shale <shalev2548@gmail.com>
2024-08-18 10:58:33 -0700

    added a system factory approach to building the swerve. added basic robot code for a default drive command for the swerve.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,13 +1,20 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.DriveWithXBox;
+import frc.robot.subsystems.Swerve;
 
 public class Robot extends TimedRobot {
-
+    Swerve swerve;
+    XboxController xboxController;
     @Override
     public void robotInit() {
-
+        swerve = SystemFactory.createSwerve();
+        xboxController = new XboxController(0);
+        DriveWithXBox driveWithXBox = new DriveWithXBox(swerve,xboxController);
+        swerve.setDefaultCommand(driveWithXBox);
     }
 
     @Override
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,6 +2,10 @@ package frc.robot;
 
 public class RobotMap {
 
-    public static final int PIGEON ;
+
+    public static final double SWEARVE_ABSOLUTE_FL_ZERO_ANGLE = 91.2;
+    public static final double SWEARVE_ABSOLUTE_FR_ZERO_ANGLE = 297.59;
+    public static final double SWEARVE_ABSOLUTE_RL_ZERO_ANGLE = 35.1;
+    public static final double SWEARVE_ABSOLUTE_RR_ZERO_ANGLE = 190;
 
 }
