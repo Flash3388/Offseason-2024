dfce18371fdbd890956147563f854c2c39cf86ed
shale <shalev2548@gmail.com>
2024-08-21 18:50:29 -0700

    changed th movewith xbox command to match the drrive func in swerv ethat works on mps

--- src/main/java/frc/robot/commands/DriveWithXBox.java
+++ src/main/java/frc/robot/commands/DriveWithXBox.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotMap;
 import frc.robot.subsystems.Swerve;
 
 public class DriveWithXBox extends Command {
@@ -16,10 +17,10 @@ public class DriveWithXBox extends Command {
 
     @Override
     public void execute() {
-        double driveY = xboxController.getLeftY();
-        double driveX = xboxController.getLeftX();
-        double rotation = -xboxController.getRightX();
-        swerve.drive(driveX,driveY,rotation);
+        double driveY = xboxController.getLeftY() * RobotMap.ATTAINBLE_MAX_SPEED_MPS_SWERVE;
+        double driveX = xboxController.getLeftX() * RobotMap.ATTAINBLE_MAX_SPEED_MPS_SWERVE;
+        double rotation = -xboxController.getRightX() *RobotMap.ATTAINBLE_MAX_SPEED_MPS_SWERVE;
+        swerve.drive(driveX ,driveY,rotation);
     }
 
     @Override
