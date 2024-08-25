239b9f70aa3bfa13bd0ac3e5ad61875ce1284af0
stavit456 <stavfrenkel456@gmail.com>
2024-08-25 16:53:56 +0300

    .

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,8 +2,8 @@ package frc.robot;
 
 public class RobotMap {
     public static final int ID_MOTOR = 13;
-    public static final int ID_LEFT = 8;
-    public static final int ID_RIGHT = 6;
+    public static final int ID_LEFT = 6;
+    public static final int ID_RIGHT = 8;
 
 
 }
