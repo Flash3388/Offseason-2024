def1ce61e7cda19b81f98199a09142fa0267d447
Daniel Inbar <daniel20102008@gmail.com>
2024-09-01 16:48:30 +0300

    created RotateShooter

---
+++ src/main/java/frc/robot/commands/RotateShooter.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class RotateShooter extends Command {
+    private Shooter shooter;
+    private double speed;
+    public RotateShooter(Shooter shooter,double speed){
+        this.shooter=shooter;
+        this.speed=speed;
+    }
+
+    @Override
+    public void initialize() {
+        shooter.resetPid();
+    }
+
+    @Override
+    public void execute() {
+        shooter.movePid(speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+       shooter.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return shooter.isAtRPM1(speed) && shooter.isAtRPM2(speed);
+    }
+
+}
