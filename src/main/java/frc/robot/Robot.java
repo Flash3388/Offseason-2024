614bec49bb23b6f49b5b08d97bccb77fd7cc7e99
Maayan <d46880@ganyavne.ort.org.il>
2024-09-09 11:52:51 +0300

    saved the climb motor's ID in the RobotMap

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -95,7 +95,6 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
-        climb.print();
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkLimitSwitch;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
 
 public class Climb extends SubsystemBase {
     private CANSparkMax motor;
@@ -13,7 +14,7 @@ public class Climb extends SubsystemBase {
     private static final double SPEED = 0.3;
 
     public Climb() {
-        motor=new CANSparkMax(18, CANSparkLowLevel.MotorType.kBrushless);
+        motor=new CANSparkMax(RobotMap.CLIMB_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless);
 
         forwardlimitswitch =  motor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
         reverselimitswitch = motor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
