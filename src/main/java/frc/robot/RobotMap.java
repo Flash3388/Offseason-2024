77b0ee5521d8efde93e3dc4f24bc83a95995b44d
stavit456 <stavfrenkel456@gmail.com>
2024-09-11 20:14:43 +0300

    I added timer to ShooterAMP.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,7 +9,7 @@ public final static double SHOOTER_LEFT_KP1 = 0.00000178;
 public final static double SHOOTER_LEFT_KP2 = 0.00018;
 public static final int SHOOTER_MOTOR_LEFT = 14;
 public static final int SHOOTER_MOTOR_RIGHT = 12;
-public static final double SHOOTER_SPEED_AMP = -2000;
+public static final double SHOOTER_SPEED_AMP = -700;
 public static final double SHOOTER_SPEED_SPEAKER = -4000;
 
 public static final int INTAKE_ID_MOTOR = 13;
--- src/main/java/frc/robot/commands/ShooterAMP.java
+++ src/main/java/frc/robot/commands/ShooterAMP.java
@@ -1,42 +1,71 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
+import java.sql.Time;
+
 public class ShooterAMP extends Command {
-    private Shooter shooter;
-    private double speed;
-    private Intake intake;
+    private final Shooter shooter;
+    private final double speed;
+    private final Intake intake;
+    private final Timer timer;
+    private final Timer fullRunTimer;
+
+    private boolean timerStarted;
+
     public ShooterAMP(Shooter shooter, double speed, Intake intake){
         this.shooter=shooter;
         this.speed=speed;
         this.intake = intake;
+        this.timer = new Timer();
+        this.fullRunTimer = new Timer();
+
         addRequirements(shooter, intake);
     }
 
     @Override
     public void initialize() {
         shooter.resetPid();
+        timer.reset();
+
+        fullRunTimer.reset();
+        fullRunTimer.start();
+
+        timerStarted = false;
     }
 
     @Override
     public void execute() {
         shooter.movePid(speed);
-        if(shooter.isAtRangePIDRight() && shooter.isAtRangePIDLeft()){
+
+        if(shooter.isAtRangePIDRight(speed) && shooter.isAtRangePIDLeft(speed)){
             intake.in();
+        }
+
+        if (!timerStarted && !intake.hasBall()) {
+            timerStarted = true;
+            SmartDashboard.putNumber("TimeToNoBall", fullRunTimer.get());
+            timer.start();
         }
     }
 
     @Override
     public void end(boolean interrupted) {
+        SmartDashboard.putNumber("TimeToEnd", fullRunTimer.get());
+
         shooter.stop();
         intake.stop();
+        timer.stop();
+        fullRunTimer.stop();
     }
 
     @Override
     public boolean isFinished() {
-        if(!intake.hasBall()){
+        if(timer.hasElapsed(0.5)){
             return true;
         }
         return false;
