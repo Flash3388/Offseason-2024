96f7dec17469d1005a3635964ea50fcad6654d6b
stavit456 <stavfrenkel456@gmail.com>
2024-09-25 19:35:22 +0300

    I added the ARM_FLOOR_ANGLE, the minimum angle.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,7 +3,7 @@ package frc.robot;
 public class RobotMap {
     public static final int ARM_MASTER = 16;
     public static final int ARM_FOLLOW = 15;
-
+    public static final double ARM_FLOOR_ANGLE  = 13.3;
 
 
 }
