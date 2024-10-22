45f5f48af94594cb3b07e715b5e4b3c1a486e14f
shale <shalev2548@gmail.com>
2024-10-21 20:53:23 -0700

    fixed autonomousInit lines, deafult command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -138,7 +138,6 @@ public class Robot extends TimedRobot {
         autoChooser = new SendableChooser<>();
         autoChooser.setDefaultOption("Do Nothing", Commands.none());
         autoChooser.addOption("Auto-working", autonomo());
-        autoChooser.addOption("Auto-notWorking",autonomoEmpty());
 
         SmartDashboard.putData("SelectAuto", autoChooser);
     }
